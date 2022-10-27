0941e4090616ba9fb318405d4104813893bc252f
designermehedi <mehedihasan80912@gmail.com>
2022-10-27 21:51:48 +0600

    blog page updated

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -16,9 +16,19 @@ const Blog = () => {
 							<Card.Body>
 								<Card.Title>1. what is cors?</Card.Title>
 								<Card.Text>
-									Some quick example text to build on the card
-									title and make up the bulk of the card's
-									content.
+									Cross-Origin Resource Sharing (CORS) is an
+									HTTP-header based mechanism that allows a
+									server to indicate any origins (domain,
+									scheme, or port) other than its own from
+									which a browser should permit loading
+									resources. CORS also relies on a mechanism
+									by which browsers make a "preflight" request
+									to the server hosting the cross-origin
+									resource, in order to check that the server
+									will permit the actual request. In that
+									preflight, the browser sends headers that
+									indicate the HTTP method and headers that
+									will be used in the actual request.
 								</Card.Text>
 							</Card.Body>
 						</Card>
@@ -37,9 +47,16 @@ const Blog = () => {
 									authentication?
 								</Card.Title>
 								<Card.Text>
-									Some quick example text to build on the card
-									title and make up the bulk of the card's
-									content.
+									The Firebase Realtime Database lets you
+									build rich, collaborative applications by
+									allowing secure access to the database
+									directly from client-side code. Data is
+									persisted locally, and even while offline,
+									realtime events continue to fire, giving the
+									end user a responsive experience. Auth0,
+									MongoDB, Passport, Okta, and Firebase are
+									the most popular alternatives and
+									competitors to Firebase Authentication.
 								</Card.Text>
 							</Card.Body>
 						</Card>
@@ -58,9 +75,20 @@ const Blog = () => {
 									3. How does the private route work?
 								</Card.Title>
 								<Card.Text>
-									Some quick example text to build on the card
-									title and make up the bulk of the card's
-									content.
+									Private Routes in React Router (also called
+									Protected Routes) require a user being
+									authorized to visit a route (read: page). So
+									if a user is not authorized for a specific
+									page, they cannot access it. The most common
+									example is authentication in a React
+									application where a user can only access the
+									protected pages when they are authorized
+									(which means in this case being
+									authenticated). Authorization goes beyond
+									authentication though. For example, a user
+									can also have roles and permissions which
+									give a user access to specific areas of the
+									application.
 								</Card.Text>
 							</Card.Body>
 						</Card>
@@ -77,9 +105,27 @@ const Blog = () => {
 									4. What is Node? How does Node work?
 								</Card.Title>
 								<Card.Text>
-									Some quick example text to build on the card
-									title and make up the bulk of the card's
-									content.
+									Node.js is an open-source backend javascript
+									runtime environment. It is a used as backend
+									service where javascript works on the
+									server-side of the application. This way
+									javascript is used on both frontend and
+									backend. Node.js runs on chrome v8 engine
+									which converts javascript code into machine
+									code, it is highly scalable, lightweight,
+									fast, and data-intensive. Node.js accepts
+									the request from the clients and sends the
+									response, while working with the request
+									node.js handles them with a single thread.
+									To operate I/O operations or requests
+									node.js use the concept of threads. Thread
+									is a sequence of instructions that the
+									server needs to perform. It runs parallel on
+									the server to provide the information to
+									multiple clients. Node.js is an event loop
+									single-threaded language. It can handle
+									concurrent requests with a single thread
+									without blocking it for one request.
 								</Card.Text>
 							</Card.Body>
 						</Card>
