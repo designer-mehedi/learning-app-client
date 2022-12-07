9b6e712a241920d1924334bb27167b148e9662b6
designermehedi <mehedihasan80912@gmail.com>
2022-12-07 21:52:56 +0600

    issues fixed

--- src/components/CourseDetails.js
+++ src/components/CourseDetails.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { useLoaderData } from "react-router-dom";
 
 const CourseDetails = () => {
-    const {title} = useLoaderData();
+    const {title, img, } = useLoaderData();
     
     return (
-        <div>
+        <div className="container">
+            <img src={img} alt="" />
             <h2>{title}</h2>
         </div>
     );
